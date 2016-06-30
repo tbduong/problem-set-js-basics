498092346286ba8843d46e1aec06e2b52d8b1386
tbduong <tiffaniduong@gmail.com>
2016-06-29 19:45:10 -0700

    Add prime function and return

--- challenges/primes.js
+++ challenges/primes.js
@@ -9,4 +9,11 @@
 
 */
 
-// YOUR CODE HERE
+function isPrime(value) {
+    for(var i = 2; i < value; i++) {
+        if(value % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
