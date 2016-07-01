67716484569e304e8536f1a05bb88b176cf37246
tbduong <tiffaniduong@gmail.com>
2016-06-30 17:31:44 -0700

    palindrome-detector.js Add for loop

--- challenges/palindrome-detector.js
+++ challenges/palindrome-detector.js
@@ -21,20 +21,21 @@
 */
 
 // YOUR CODE HERE
+
+//  for( var i = 0; i < palindrome.length/2; i++) {
+//        if( palindrom[i] === palindrom.charAt(palindrom.length-1))
+//        {
+//          document.write('the word is palindrome.');
+//            }else{
+//              document.write('the word is not palindrome!');
+//          }
+//      }
+//  }
+////  checkPalindrom('wordthatwillbechecked');
 function isPalindrome(){
-  //for( var i = palindrom.length; i > 0; i-- )
-      //{
-        //  if( palindrom[i] = palindrom.charAt(palindrom.length)-1 )
-          //{
-          //    document.write('the word is palindrome.');
-          //}else{
-          //    document.write('the word is not palindrome!');
-          }
-      }
+  for ( var i=0; i <palindrome.length/2; i++) {
+
   }
-//  checkPalindrom('wordthatwillbechecked');
-
-
 
 
 }
